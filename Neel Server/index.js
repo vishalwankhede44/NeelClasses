bf08267590bf6f7244fa05dc057b70ec094a86af
vishalwankhede44 <vishalwankhede44@gmail.com>
2020-08-03 19:07:34 +0530

    Enroll Add

--- Neel Server/index.js
+++ Neel Server/index.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const videoRouter = require('./routers/videoRouter');
-const courseAddRouter = require('./routers/courseAddRouter');
-const loginRouter  = require('./routers/auth');
-const signUpRouter = require('./routers/signup');
-const addUserRouter = require('./routers/adduser');
-const docRouter = require('./routers/docRouter');
-const notesRouter = require('./routers/viewNotesRouter');
-const playVideo = require('./routers/playVideoRouter');
-const searchRouter = require('./routers/searchRouter');
-const courseRouter = require('./routers/courseRouter');
-const allCoursesRouter = require('./routers/allCoursesRouter');
-const getUserRouter = require('./routers/getUser');
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+const path = require("path");
+const videoRouter = require("./routers/videoRouter");
+const courseAddRouter = require("./routers/courseAddRouter");
+const loginRouter = require("./routers/auth");
+const signUpRouter = require("./routers/signup");
+const addUserRouter = require("./routers/adduser");
+const docRouter = require("./routers/docRouter");
+const notesRouter = require("./routers/viewNotesRouter");
+const playVideo = require("./routers/playVideoRouter");
+const searchRouter = require("./routers/searchRouter");
+const courseRouter = require("./routers/courseRouter");
+const allCoursesRouter = require("./routers/allCoursesRouter");
+const getUserRouter = require("./routers/getUser");
+const enrollCourseRouter = require("./routers/enrollCourse");
+const checkAccess = require("./routers/checkAccess");
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 // cors = require("cors");
 // app.use(cors());
@@ -23,37 +25,37 @@ app.set('view engine', 'html');
 // app.use('/',indexRouter);
 // app.use('/',router);
 
-const logger = require('morgan');
-const cors = require('cors');
-
+const logger = require("morgan");
+const cors = require("cors");
 
 //use cors to allow cross origin resource sharing
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/login',loginRouter);
-app.use('/uploadVideo',videoRouter);
-app.use('/signup',signUpRouter)
-app.use('/addcourse',courseAddRouter);
-app.use('/adduser',addUserRouter);
-app.use('/uploadnotes',docRouter);
-app.use('/notes',notesRouter);
-app.use('/video',playVideo);
-app.use('/courses/search',searchRouter);
-app.use('/course',courseRouter);
-app.use('/courses',allCoursesRouter);
-app.use('/getUser',getUserRouter);
+app.use("/login", loginRouter);
+app.use("/uploadVideo", videoRouter);
+app.use("/signup", signUpRouter);
+app.use("/addcourse", courseAddRouter);
+app.use("/adduser", addUserRouter);
+app.use("/uploadnotes", docRouter);
+app.use("/notes", notesRouter);
+app.use("/video", playVideo);
+app.use("/courses/search", searchRouter);
+app.use("/course", courseRouter);
+app.use("/courses", allCoursesRouter);
+app.use("/getUser", getUserRouter);
+app.use("/enrollCourse", enrollCourseRouter);
+app.use("/checkAccess", checkAccess);
 // app.use('/getCourse',getCourse);
 
-
-app.listen(5000,() =>{
-    console.log("Server Started");
+app.listen(5000, () => {
+  console.log("Server Started");
 });
--- src/pages/Courses/CourseHeading.js
+++ src/pages/Courses/CourseHeading.js
@@ -1,180 +1,329 @@
-import React from "react"
-import { browserHistory} from 'react-router'
+import React, { useState } from "react";
+import { browserHistory } from "react-router";
 // import CourseInfo from "./course_info"
+import axios from "axios";
 
-import { faStar, faStarHalfAlt, faComment, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar,
+  faStarHalfAlt,
+  faComment,
+  faPlayCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCookies } from "react-cookie";
+
 // import CourseFeed from "./course_feed";
 
-class CourseHeading extends React.Component {
-    constructor(props) {
-        super(props)
+const CourseHeading = (props) => {
+  const [cookie, setCookie, removeCookie] = useCookies([
+    "uid",
+    "name",
+    "mobile",
+  ]);
 
-        this.renderRating = this.renderRating.bind(this)
-        // this.renderPrice = this.renderPrice.bind(this)
-        this.videoOpenHandle = this.videoOpenHandle.bind(this)
+  function enroll() {
+    console.log("I got clicked");
+
+    const uid = { cookie }.cookie.uid;
+    const courseId = props.course.courseId;
+    const Info = {
+      uid: uid,
+      courseId: courseId,
+    };
+
+    console.log(Info);
+    axios
+      .post("http://localhost:5000/enrollCourse", Info)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data == "Already") alert("Course already purchased");
+        else alert("Course purchased successfully");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  function renderRating() {
+    if (props.course.courseRating < 1) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon
+              style={{ color: "#f4c150" }}
+              icon={faStarHalfAlt}
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
     }
-
-    renderRating() {
-        if (this.props.course.courseRating < 1) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStarHalfAlt} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating == 1) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-     
-               </div>
-            )
-        }
-        if (this.props.course.courseRating > 1 && this.props.course.courseRating < 2) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStarHalfAlt} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating == 2) {
-            return (
-                <div>
-                    <span className="rating2"><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span className="rating2"><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating > 2 && this.props.course.courseRating < 3) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStarHalfAlt} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating == 3) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating > 3 && this.props.course.courseRating < 4) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStarHalfAlt} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating == 4) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating > 4 && this.props.course.courseRating < 5) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStarHalfAlt} /></span>
-                </div>
-            )
-        }
-        if (this.props.course.courseRating == 5) {
-            return (
-                <div>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                    <span><FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} /></span>
-                </div>
-            )
-        }
+    if (props.course.courseRating == 1) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
     }
-
-
-
-    // renderPrice() {
-    //     let str = this.props.course.coursePrice.toString();
-    //     return str.slice(0, str.length - 2) + "." + str.slice(str.length - 2)
-    // }
-
-
-    videoOpenHandle() {
-        let courseId = Object.values(this.props.videos)[0][0].course_content_id
-        this.props.showPlayer(0, courseId)
+    if (props.course.courseRating > 1 && props.course.courseRating < 2) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              style={{ color: "#f4c150" }}
+              icon={faStarHalfAlt}
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
     }
+    if (props.course.courseRating == 2) {
+      return (
+        <div>
+          <span className="rating2">
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span className="rating2">
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating > 2 && props.course.courseRating < 3) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              style={{ color: "#f4c150" }}
+              icon={faStarHalfAlt}
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating == 3) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating > 3 && props.course.courseRating < 4) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              style={{ color: "#f4c150" }}
+              icon={faStarHalfAlt}
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating == 4) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "dedfe0" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating > 4 && props.course.courseRating < 5) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              style={{ color: "#f4c150" }}
+              icon={faStarHalfAlt}
+            />
+          </span>
+        </div>
+      );
+    }
+    if (props.course.courseRating == 5) {
+      return (
+        <div>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+          <span>
+            <FontAwesomeIcon style={{ color: "#f4c150" }} icon={faStar} />
+          </span>
+        </div>
+      );
+    }
+  }
 
+  // renderPrice() {
+  //     let str = props.course.coursePrice.toString();
+  //     return str.slice(0, str.length - 2) + "." + str.slice(str.length - 2)
+  // }
 
-    render() {
-        return (
-            <div>
-                {this.props.course && 
-                    <div>
-                        <div className="course-header" >
-                        <div className="course-header-inner">
-                            <div className="course-info">
-                                <div>
-                                    <div className="course-header-title">{this.props.course.courseName}</div>
-                                    <div className="course-header-description">qwer wer erty tyu yui ghj fghjkl sdfghj.</div>
-                                </div>
-                                <div>
-                                    <div className="course-header-instructor">Isaal Wankide</div>
-                                    <div><span className="course-header-rating">{this.renderRating()}</span></div>
-                                </div>
-                                
-                                
-                            </div>
-                            
-                            <div className="course-description">
-                                <div>
-                                    <button type="submit" className="btn-enroll">Enroll for<br></br>$2499/-</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>  
-                }
+  function videoOpenHandle() {
+    let courseId = Object.values(props.videos)[0][0].course_content_id;
+    props.showPlayer(0, courseId);
+  }
+
+  return (
+    <div>
+      {props.course && (
+        <div>
+          <div className="course-header">
+            <div className="course-header-inner">
+              <div className="course-info">
+                <div>
+                  <div className="course-header-title">
+                    {props.course.courseName}
+                  </div>
+                  <div className="course-header-description">
+                    qwer wer erty tyu yui ghj fghjkl sdfghj.
+                  </div>
+                </div>
+                <div>
+                  <div className="course-header-instructor">Isaal Wankide</div>
+                  <div>
+                    <span className="course-header-rating">
+                      {renderRating()}
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="course-description">
+                <div>
+                  <button
+                    type="submit"
+                    className="btn-enroll"
+                    onClick={() => enroll()}
+                  >
+                    Enroll for<br></br>$2499/-
+                  </button>
+                </div>
+              </div>
             </div>
-        )
-    }
-}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default CourseHeading;
