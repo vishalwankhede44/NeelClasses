3ac599e8a3dad88773645e662a8dbfb85c27b797
vishalwankhede44 <vishalwankhede44@gmail.com>
2020-08-18 12:22:04 +0530

    Payment Test Add

--- Neel Server/routers/payment.js
+++ Neel Server/routers/payment.js
@@ -1,42 +1,44 @@
 const checksum_lib = require("../paytm/checksum/checksum");
+const express = require("express");
+const payment = express.Router();
 
-module.exports = (payment) => {
-  payment.get("/", (req, res) => {
-    let params = {};
-    (params["MID"] = "ZKBVLi06192328204943"),
-      (params["WEBSITE"] = "WEBSTAGING"),
-      (params["CHANNEL_ID"] = "WEB"),
-      (params["INDUSTRY_TYPE_ID"] = "Retail"),
-      (params["ORDER_ID"] = "ORD000"),
-      (params["CUST_ID"] = "CUST0011"),
-      (params["TXN_AMOUNT"] = "100"),
-      (params["CALLBACK_URL"] = "http://localhost:5000/callback"),
-      (params["EMAIL"] = "xyz@gmail.com"),
-      (params["MOBILE_NO"] = "7875927143");
-    checksum_lib.genchecksum(params, "aZMUiL7OM%nvw@JN", function (
-      err,
-      checksum
-    ) {
-      let txn_url = "https://securegw-stage.paytm.in/order/process";
+payment.get("/", (req, res) => {
+  console.log("IN PAYTM");
+  let params = {};
+  (params["MID"] = "ZKBVLi06192328204943"),
+    (params["WEBSITE"] = "WEBSTAGING"),
+    (params["CHANNEL_ID"] = "WEB"),
+    (params["INDUSTRY_TYPE_ID"] = "Retail"),
+    (params["ORDER_ID"] = "ORD000"),
+    (params["CUST_ID"] = "CUST0011"),
+    (params["TXN_AMOUNT"] = "100"),
+    (params["CALLBACK_URL"] = "http://localhost:5000/callback"),
+    (params["EMAIL"] = "xyz@gmail.com"),
+    (params["MOBILE_NO"] = "7875927143");
+  checksum_lib.genchecksum(params, "aZMUiL7OM%nvw@JN", function (
+    err,
+    checksum
+  ) {
+    let txn_url = "https://securegw-stage.paytm.in/order/process";
 
-      let form_fields = "";
-      for (x in params) {
-        form_fields +=
-          "<input type='hidden' name='" + x + "' value='" + params[x] + "'/>";
-      }
-
+    let form_fields = "";
+    for (x in params) {
       form_fields +=
-        "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "'/>";
+        "<input type='hidden' name='" + x + "' value='" + params[x] + "'/>";
+    }
 
-      var html =
-        '<html><body><center><h1>Please wait! Do not refresh the page</h1></center><form method="post" action="' +
-        txn_url +
-        '" name="f1">' +
-        form_fields +
-        '</form><script type="text/javascript">document.f1.submit</script></body></html>';
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
-    });
+    form_fields +=
+      "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "'/>";
+
+    var html =
+      '<html><body><center><h1>Please wait! Do not refresh the page</h1></center><form method="post" action="' +
+      txn_url +
+      '" name="f1">' +
+      form_fields +
+      '</form><script type="text/javascript"> document.f1.submit() </script></body></html>';
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.end();
   });
-};
+});
+module.exports = payment;
--- src/pages/Home/HomePanel.js
+++ src/pages/Home/HomePanel.js
@@ -1,73 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import  {withRouter} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import axios from "axios";
 
 class HomeSearchPanel extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            searchString: "",
-            iconClassName: "search-icon",
-        }
-        this.inputFocus = this.inputFocus.bind(this)
-        this.inputUnFocus = this.inputUnFocus.bind(this)
-        this.getSearchInput = this.getSearchInput.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: "",
+      iconClassName: "search-icon",
+    };
+    this.inputFocus = this.inputFocus.bind(this);
+    this.inputUnFocus = this.inputUnFocus.bind(this);
+    this.getSearchInput = this.getSearchInput.bind(this);
+  }
+  getSearchInput(e) {
+    if (e.key === "Enter") {
+      this.searchClickHandle();
+    } else {
+      this.setState({
+        searchString: e.target.value,
+      });
     }
-    getSearchInput(e) {
-        if (e.key === 'Enter') {
-            this.searchClickHandle();
-        } else {
-            this.setState({
-                searchString: e.target.value
-            })
-        }
-    }
-    searchClickHandle() {
-        this.props.history.push(`/courses/search/${this.state.searchString}`);
-    }
+  }
+  searchClickHandle() {
+    this.props.history.push(`/courses/search/${this.state.searchString}`);
+  }
 
-    inputFocus() {
-        this.setState({
-            iconClassName: "search-icon-open"
-        })
-    }
+  inputFocus() {
+    this.setState({
+      iconClassName: "search-icon-open",
+    });
+  }
 
-    inputUnFocus() {
-        this.setState({
-            iconClassName: "search-icon"
-        })
-    }
+  inputUnFocus() {
+    this.setState({
+      iconClassName: "search-icon",
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="home-box">
-                        <div className="home-content">
-                            <p className="home-headline">The Premium System Education</p>
-                            <p className="home-subline">Future of Education Technology</p>
-                            <div class="wrap">
-                                <div class="search">
-                                    <input type="text" class="searchTerm" placeholder="Search for anything" onFocus={this.inputFocus} onBlur={this.inputUnFocus} onKeyUp={this.getSearchInput}></input>
-                                    <button type="submit" class="searchButton">
-                                        <FontAwesomeIcon icon={faSearch} className={this.state.iconClassName} />
-                                    </button>
-                                </div>
-                            </div>
-                            
-                            <div className="home-button-new">
-                                <button class="btn-in">Log In</button>
-                                <button class="btn-up">Sign Up</button>
-                                
-                            </div>
-                        </div>
-                        </div>
-                </div>
-            
-        )
-    }
+  checkP() {
+    console.log("CHECK P");
+    axios.get("http://localhost:5000/payment").then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+        console.error(err);
+      });;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="home-box">
+          <div className="home-content">
+            <p className="home-headline">The Premium System Education</p>
+            <p className="home-subline">Future of Education Technology</p>
+            <div class="wrap">
+              <div class="search">
+                <input
+                  type="text"
+                  class="searchTerm"
+                  placeholder="Search for anything"
+                  onFocus={this.inputFocus}
+                  onBlur={this.inputUnFocus}
+                  onKeyUp={this.getSearchInput}
+                ></input>
+                <button type="submit" class="searchButton">
+                  <FontAwesomeIcon
+                    icon={faSearch}
+                    className={this.state.iconClassName}
+                  />
+                </button>
+              </div>
+            </div>
+
+            <div className="home-button-new">
+              <button onClick={this.checkP} class="btn-in">
+                Payment
+              </button>
+              <button class="btn-up">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default withRouter(HomeSearchPanel);
